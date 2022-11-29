2f17b4332acf69eee4a91d53724ce9793c4649d0
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-11-29 15:38:59 +0100

    Hero images added
    Hero images added!

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "../Header/Header";
 import './Hero.css'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
 const Hero = () => {
     return (
     <div className="hero">
@@ -54,7 +58,21 @@ const Hero = () => {
         </div>
 
         </div>
-       <div className="right-h"></div>
+       <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>117 bpm</span>
+        </div>
+
+        {/* hero images */}
+
+        <img src={hero_image} alt="" className="hero-image"/>
+        <img src={hero_image_back} alt="" className="hero-image-back"/>
+
+       </div>
     </div>
     )
 }
