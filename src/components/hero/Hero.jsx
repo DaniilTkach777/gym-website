6b4bcb9168d02410928247339612e4eadc188575
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-07 17:45:24 +0100

    New design added!
    New design added

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import Header from "../Header/Header";
+import './Hero.css'
+import hero_image from "../../assets/hero_image.png"
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from "../../assets/calories.png"
+const Hero = () => {
+    return (
+    <div className="hero">
+       <div className="left-h">
+        <Header/>
+{/* the best ad */}
+        <div className="the-best-ad">
+        <div></div>
+        <span>The best Gym in the town!</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+            <div>
+                <span className="stroke-text">Shape</span>
+                <span> Your</span>
+                
+            </div>
+            <div>
+                <span>Ideal body!</span>
+            </div>
+            <div>
+              <span>
+            In here we will help you to shape and build your ideal body and live up your life to fullest
+              </span>
+            </div>
+        </div>
+
+        {/* figures */}
+
+        <div className="figures">
+          <div>
+            <span>+157</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+957</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+75</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* hero buttons */}
+
+        <div className="hero-buttons">
+          <buttons className="btn">Get Started</buttons>
+          <buttons className="btn">Learn More</buttons>
+        </div>
+
+        </div>
+       <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>117 bpm</span>
+        </div>
+
+        {/* hero images */}
+
+        <img src={hero_image} alt="" className="hero-image"/>
+        <img src={hero_image_back} alt="" className="hero-image-back"/>
+        {/* calories */}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>230 kcal</span>
+          </div>
+        </div>
+
+       </div>
+    </div>
+    )
+}
+
+export default Hero
