a393f039aec1fcdafa0cb10767e582cb92639e71
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-08 11:45:45 +0100

    PROGRAMS added
    PROGRAMS added

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import './Programs.css'
-import {programsData} from '../../data/programsData.js'
-import rightArrow from '../../assets/rightArrow.png'
+import React from 'react' ;
+import './Programs.css' ;
+import { programsData } from "../../data/programsData.js" ;
+import RightArrow from '../../assets/rightArrow.png'
+
 const Programs = () => {
     return (
         <div className="Programs" id="programs">
             {/* header */}
+            
             <div className="programs-header">
                 <span className='stroke-text'>Explore Our</span>
                 <span>Programs</span>
                 <span className='stroke-text'>To Shape You</span>
             </div>
-
-            
-            
-            <div className="prgoram-categories">
+        <div className="program-categories">
             {programsData.map ((program)=>(
              <div className="category">
                 {program.image}
                 <span>{program.heading}</span><span>{program.details}</span>
                 <div className="join-now">
-                    <span>Join Now</span> <img src="{RightArrow}" alt="" />
+                    <span>Join Now</span> <img src={RightArrow} alt="" />
                 </div>
              </div>
           ))}
