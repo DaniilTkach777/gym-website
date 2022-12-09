05cb66d00e58116fe2608d11479e86a455039d8e
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-09 10:25:52 +0100

    Partners added

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -5,7 +5,8 @@ import image2 from "../../assets/image2.png" ;
 import image3 from "../../assets/image3.png" ;
 import image4 from "../../assets/image4.png" ;
 import nb from '../../assets/nb.png' ;
-import adidas from '../../assets/nike.png' ;
+import adidas from '../../assets/adidas.png' ;
+import nike from '../../assets/nike.png'
 import tick from "../../assets/tick.png" ;
 const Reasons = () => {
     return (
@@ -27,20 +28,34 @@ const Reasons = () => {
                 <div className='details-r'>
                     <div>
                        <img src={tick} alt=""></img>
-                       <span></span>
+                       <span>OVER 140+ EXPERT COACHS</span>
                     </div>
                     <div>
                        <img src={tick} alt="" />
-                       <span></span>
+                       <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
                     </div>
                     <div>
                        <img src={tick} alt="" />
-                       <span></span>
+                       <span>1 FREE PROGRAM FOR NEW MEMBER</span>
                     </div>
                     <div>
                        <img src={tick} alt="" />
-                       <span></span>
+                       <span>RELIABLE PARTNERS</span>
                     </div>
+                </div>
+
+                <span style={{
+                    color : 'var(--gray)',
+                    fontWeight: "normal",
+                }}>OUR PARTNERS
+                </span>
+
+                <div className="partners">
+
+                   <img src={nb} alt="" />
+                   <img src={adidas} alt="" />
+                   <img src={nike} alt="" />
+                
                 </div>
             </div>
         </div>
