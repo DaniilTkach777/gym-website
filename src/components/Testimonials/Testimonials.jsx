a2c6a8654d151bf116153b1fc763b2ea750a47ea
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-10 09:48:37 +0100

    Testimonials added

---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './Testimonials.css';
+import { testimonialsData } from "../../data/testimonialsData";
+const Testimonials = () => {
+    return (
+        <div className="Testimonials">
+            <div className="left-t">
+                <span>Testimonials</span>
+                <span className="stroke-text">What they</span>
+                <span>say about us</span>
+                <span>
+                    {testimonialsData[0].review}
+                </span>
+            </div>
+            <div className="right-t"></div>
+        </div>
+    )
+}
+
+export default Testimonials
