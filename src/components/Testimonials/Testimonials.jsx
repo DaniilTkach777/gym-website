9c758ae7f652a4022330046d3e02186182c5824c
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-10 10:31:14 +0100

    Testimonials 1

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import './Testimonials.css';
 import { testimonialsData } from "../../data/testimonialsData";
+import { useState } from "react";
+import leftArrow from '../../assets/leftArrow.png' ;
+import rightArrow from '../../assets/rightArrow.png' ;
 const Testimonials = () => {
+
+    const [selected, setSelected] = useState(0);
+    const tLength = testimonialsData.length;
     return (
         <div className="Testimonials">
             <div className="left-t">
@@ -9,10 +15,23 @@ const Testimonials = () => {
                 <span className="stroke-text">What they</span>
                 <span>say about us</span>
                 <span>
-                    {testimonialsData[0].review}
+                    {testimonialsData[selected].review}
+                </span>
+                <span>
+                    <span style={{color: '#15f423'}}>
+                        {testimonialsData[selected].name}
+                    </span> {" "}
+                    - {testimonialsData[selected].status}
                 </span>
             </div>
-            <div className="right-t"></div>
+            <div className="right-t">
+                <img src={testimonialsData[selected].image} alt="" />
+
+                <div className="arrows">
+                    <img src={leftArrow} alt="" />
+                    <img src={rightArrow} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
