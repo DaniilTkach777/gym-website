67910bce3309f6f9b20e0efe825c3fc03814999f
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-15 12:01:12 +0100

    Hero responsive added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ import {motion} from 'framer-motion'
 const Hero = () => {
 
   const trasition = {type:'spring', duration: 3}
+  const mobile = window.innerWidth<=768 ? true: false;
     return (
     <div className="hero">
 
@@ -18,7 +19,7 @@ const Hero = () => {
 {/* the best ad */}
         <div className="the-best-ad">
         <motion.div
-        initial={{left:'238px'}}
+        initial={{left: mobile? "165px": '238px'}}
         whileInView={{left: '8px'}}
         transition={ {...trasition, type: 'tween'} }
         
