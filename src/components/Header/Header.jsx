2d7270a298dc50bcb45acb89413ab7c227e2ffe8
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-16 12:14:34 +0100

    Auto scroll 2 added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,16 +24,52 @@ const Header = () => {
                     
                     <img src={Bars} alt="" style={{width:'1.5rem', height:'1.5rem'}}/></div>
             ): <ul className="header-menu">
-            <li onClick={()=>setMenuOpened(false)}><Link>Home</Link></li>
-            <li onClick={()=>setMenuOpened(false)}>Programs</li>
-            <li onClick={()=>setMenuOpened(false)}>Why us</li>
-            <li onClick={()=>setMenuOpened(false)}><Link>Plans</Link></li>
-            <li onClick={()=>setMenuOpened(false)}><Link
-            to="Testimonials"
+            <li onClick={()=>setMenuOpened(false)}>
+                
+            <Link
+            onClick={()=>setMenuOpened(false)}
+            to="header"
+            span={true}
+            smooth={true}
+            >Home</Link>
+            </li>
+            <li>
+
+            <Link
+            onClick={()=>setMenuOpened(false)}
+            to="programs"
             span={true}
             smooth={true}
             
-            >Testimonials</Link></li>
+            >Programs</Link></li>
+            <li>
+                
+            <Link 
+            onClick={()=>setMenuOpened(false)}
+            to="reasons"
+            span={true}  
+            smooth={true}
+            
+            >Why us</Link></li>
+            <li>
+            
+            <Link
+            onClick={()=>setMenuOpened(false)}
+            to="plans"
+            span={true}
+            smooth={true}
+            >Plans</Link>
+            </li>
+
+            <li>
+                
+            <Link
+            onClick={()=>setMenuOpened(false)}
+            to="Testimonials"
+            span={true}
+            smooth={true}
+            >Testimonials</Link>
+            </li>
         </ul>}
             
         </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
   const trasition = {type:'spring', duration: 3}
   const mobile = window.innerWidth<=768 ? true: false;
     return (
-    <div className="hero">
+    <div className="hero" id='home'>
 
       <div className="blur hero-blur"></div>
        <div className="left-h">
