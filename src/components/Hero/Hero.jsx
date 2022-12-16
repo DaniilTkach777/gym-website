bd14febaedbd6905f9e27a70e8f8f8aed0e1d609
DaniilTkach777 <112399818+DaniilTkach777@users.noreply.github.com>
2022-12-16 13:36:08 +0100

    Counting Numbers added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,6 +5,7 @@ import hero_image from "../../assets/hero_image.png";
 import hero_image_back from "../../assets/hero_image_back.png";
 import Heart from "../../assets/heart.png";
 import Calories from "../../assets/calories.png";
+import NumberCounter from 'number-counter'
 import {motion} from 'framer-motion'
 const Hero = () => {
 
@@ -16,7 +17,7 @@ const Hero = () => {
       <div className="blur hero-blur"></div>
        <div className="left-h">
         <Header/>
-{/* the best ad */}
+    {/* the best ad */}
         <div className="the-best-ad">
         <motion.div
         initial={{left: mobile? "165px": '238px'}}
@@ -50,15 +51,23 @@ const Hero = () => {
 
         <div className="figures">
           <div>
-            <span>+157</span>
+            <span>
+              
+              <NumberCounter end={157} start={100} delay='4' preFix="+"/>
+              </span>
             <span>expert coachs</span>
           </div>
           <div>
-            <span>+957</span>
+            <span>
+              
+              
+            <NumberCounter end={957} start={800} delay='4' preFix="+"/></span>
             <span>members joined</span>
           </div>
           <div>
-            <span>+75</span>
+            <span>
+              
+            <NumberCounter end={45} start={30} delay='4' preFix="+"/></span>
             <span>fitness programs</span>
           </div>
         </div>
